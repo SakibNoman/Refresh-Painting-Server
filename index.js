069c3c8ce6f5a8e2312063f3eb68f29ae42fc7b4
Sakib Noman <skbnmn@gmail.com>
2021-04-16 21:19:37 +0600

    add delete service api

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
+const ObjectID = require('mongodb').ObjectID;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqpfg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 
 const app = express()
 
@@ -14,5 +18,33 @@ const port = 5000;
 app.get('/', (req, res) => {
     res.send("hello from db it's working working")
 })
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const servicesCollection = client.db("refreshdb").collection("services");
+
+    app.post('/addService', (req, res) => {
+        const service = req.body;
+        servicesCollection.insertOne(service)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/services', (req, res) => {
+        servicesCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.delete('/deleteService/:id', (req, res) => {
+        const id = ObjectID(req.params.id)
+        servicesCollection.findOneAndDelete({ _id: id })
+            .then(res => res.json())
+            .then(data => console.log("successfully deleted"))
+    })
+
+});
 
 app.listen(process.env.PORT || port)
